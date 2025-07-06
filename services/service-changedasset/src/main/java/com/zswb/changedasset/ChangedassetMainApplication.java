bc8f254d9bacf75aef52d230a5ded26c51fe0529
itcast <hello@itcast.cn>
2025-07-06 20:55:37 +0800

    项目基础架构设置

---
+++ services/service-changedasset/src/main/java/com/zswb/changedasset/ChangedassetMainApplication.java
@@ -0,0 +1,11 @@
+package com.zswb.changedasset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangedassetMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChangedassetMainApplication.class, args);
+    }
+}
---
+++ services/service-changedasset/src/main/java/com/zswb/changedasset/controller/ChangedassetController.java
@@ -0,0 +1,13 @@
+package com.zswb.changedasset.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ChangedassetController {
+
+
+}
